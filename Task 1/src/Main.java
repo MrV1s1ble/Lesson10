3081dcd8fd53f87a7fe7465734532d0d6560ec26
crin706 <rosh-ka@mail.ru>
2024-04-25 23:53:38 +0300

    done

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
     static String booksInfo = "";
 
+//    Добавить в программу метод, который будет в информацию о книге добавлять: 
+//    Название книги, кол-во страниц, цену книги в рублях. 
+//    Если у книги нет цены, то надо писать “не установлено”.
 
     public static void main(String[] args) {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
+            int price = inputPrice();
             addBook(name);
             addBook(name, pageCount);
-            //todo использовать новый метод тут
+            addBook(name, pageCount, price);
             printInfo();
         }
     }
+
 
     public static String inputBookName() {
         System.out.println("Введите название книги:");
@@ -25,16 +30,25 @@ public class Main {
         return new Scanner(System.in).nextInt();
     }
 
+    public static int inputPrice() {
+        System.out.println("Введите стоимость книги:");
+        return new Scanner(System.in).nextInt();
+    }
+
     public static void addBook(String bookName) {
         addBook(bookName, 0);
     }
 
     public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+        addBook(bookName, pageCount, 0);
+    }
+
+    private static void addBook(String bookName, int pageCount, int price) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр. Стоимость книги: "
+                + (price > 0 ? price : "не установлено") + " руб.";
     }
 
     public static void printInfo() {
         System.out.println(booksInfo);
     }
-
 }
